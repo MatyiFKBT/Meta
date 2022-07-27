f5d0edb422c61162125331463963564cdaaccfeb
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2022-07-27 10:57:43 +0000

    ci: add label to PR automatically

--- src/compare.ts
+++ src/compare.ts
@@ -87,9 +87,12 @@ function mutateForBetterStringify(
     newDBTypes.set(type.name, fjss(mutateForBetterStringify(newTypesName, newGroupsName, type)));
   }
 
+  let changed = false;
+
   for (const type of oldDBTypes.keys()) {
     if (!newDBTypes.has(type)) {
       console.log(chalk`{gray [}{red Removed}{gray ]} ${type}`);
+      changed = true;
     }
   }
 
@@ -97,10 +100,15 @@ function mutateForBetterStringify(
     if (!oldDBTypes.has(type)) {
       console.log(chalk`{gray [}{green Added}{gray ]} ${type}`);
       console.log(chalk.italic.gray`${newDBTypes.get(type)}`);
+      changed = true;
     } else if (oldDBTypes.get(type) !== newDBTypes.get(type)) {
       console.log(chalk`{gray [}{blue Edited}{gray ]} ${type}`);
       console.log(chalk.gray.italic`Previously:\n${oldDBTypes.get(type)}`);
       console.log(chalk.gray.italic`Now: \n${newDBTypes.get(type)}`);
+      changed = true;
     }
+  }
+  if (!changed) {
+    console.log(chalk.green("No changes detected"));
   }
 })();
